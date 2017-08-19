20497f19bcfadc5ab1838c0c689acf200a40f93a
Alissid <essid.aly@gmail.com>
2017-08-18 17:34:46 -0700

    Update resumeBuilder.js

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -126,7 +126,7 @@ var projects = {
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
-var formattedwelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
 var formattedContactInfo = [];
 formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
